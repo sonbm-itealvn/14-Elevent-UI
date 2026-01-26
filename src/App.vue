5903382b329db4cf28e1e992ed1630e0d77b5aa8
sonbm-itealvn <sonbm.itealvn@gmail.com>
2026-01-26 11:16:46 +0700

    Notification when update version

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,13 @@ import { computed } from 'vue';
 import { RouterView } from 'vue-router';
 import { NConfigProvider, lightTheme, darkTheme, NLoadingBarProvider, NNotificationProvider, NMessageProvider } from 'naive-ui';
 import useThemeStore from '@/ui/stores/theme.store';
+import { useAppUpdate } from '@/shared/composable/useAppUpdate';
 
 const themeStore = useThemeStore();
-
 const theme = computed(() => themeStore.getTheme);
+
+// Enable auto-update checking
+useAppUpdate();
 
 </script>
 
